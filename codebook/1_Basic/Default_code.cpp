b5488861ec654f6feff2ac8bf3d7a4259863d590
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-09-05 23:49:15 +0800

    add 1_Basic

---
+++ codebook/1_Basic/Default_code.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve()
+{
+
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+      solve();
+  }
+  return 0;
+}
---
+++ codebook/1_Basic/check.sh
@@ -0,0 +1,7 @@
+for i in $(seq 1 10000);
+do
+  ./gen > input
+  ./ac < input > out_ac
+  ./wa < input > out_wa
+  diff out_ac out_wa || break
+done
