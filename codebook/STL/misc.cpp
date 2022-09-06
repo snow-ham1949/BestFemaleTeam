e63bccefe95182096f434a20b9508538464cc368
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-09-06 22:52:38 +0800

    fix format

--- codebook/STL/misc.cpp
+++ codebook/STL/misc.cpp
@@ -1,18 +1,18 @@
 // return iterator, find first >= val
-lower_bound(vector.begin(), vector.end(), val);
+lower_bound(vector.begin(), vector.end(), val)
 
 // return iterator, find find > val
-upper_bound(vector.begin(), vector.end(), val);
+upper_bound(vector.begin(), vector.end(), val)
 
 // array
 min_element(arr, arr + n); // 0-based
 // vector, return iterator
-min_element(vector.begin(), vector.end());
+min_element(vector.begin(), vector.end())
 
 // array
 max_element(arr, arr + n); // 0-based
 // vector, return iterator
-max_element(vector.begin(), vector.end());
+max_element(vector.begin(), vector.end())
 
 // fix nth element at nth index
 nth_element(vector.begin(), vector.begin() + 4, vector.end()) // fix 5th element at vector[4]
@@ -24,4 +24,4 @@ do {
 // vector
 do {
 
-} while(next_permutation(vector.begin(), vector.end()));
+} while(next_permutation(vector.begin(), vector.end()))
