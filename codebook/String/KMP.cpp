d738f6c8133966cd5794411cdb08f05fd2521308
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-09-08 01:19:57 +0800

    unified format

--- codebook/String/KMP.cpp
+++ codebook/String/KMP.cpp
@@ -16,21 +16,19 @@ void Prefix(string &Pattern)
 vector<int> KMP(string Text, string Pattern){
   // all 1-indexed
   int lT = Text.size(), lP = Pattern.size();
-	Text = "#" + Text;
+  Text = "#" + Text;
   Pattern = "#" + Pattern;
   vector<int> ans;
-	Prefix(Pattern);
-	for(int i = 1, j = 0; i <= lT; i++){
-		while(j > 0 && Pattern[j+1] != Text[i])
-			j = pi[j];
-		if (Pattern[j+1] == Text[i])
-			j ++;
-		if (j == lP) 
-		{
+  Prefix(Pattern);
+  for(int i = 1, j = 0; i <= lT; i++){
+    while(j > 0 && Pattern[j + 1] != Text[i]) j = pi[j];
+    if (Pattern[j + 1] == Text[i]) j++;
+    if (j == lP) 
+    {
       // 如果match到了, 出現位置為 i-k (1-indexed)
-			j = pi[j];
+      j = pi[j];
       ans.push_back(i-lP+1);
-		} 
-	}
+    } 
+  }
   return ans;
 }
