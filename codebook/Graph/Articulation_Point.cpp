f11b801c52ddffa214d545e6fc7c851c91932567
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-09-09 20:30:33 +0800

    fix format

--- codebook/Graph/Articulation_Point.cpp
+++ codebook/Graph/Articulation_Point.cpp
@@ -1,4 +1,5 @@
 const int mxn = 2e4 + 5;
+
 vector<int> g[mxn];
 int cnt = 0, ap_num = 0;
 int dfn[mxn], low[mxn], vis[mxn], is_ap[mxn];
@@ -27,5 +28,15 @@ void tarjan(int u, int fa)
   if (fa == -1 && child > 1 && !is_ap[u]) {
     is_ap[u] = 1;
     ap_num++;
+  }
+}
+
+void solve()
+{
+  for (int i = 1; i <= n; i++)
+  {
+    if (!vis[i]) {
+      tarjan(i, -1);
+    }
   }
 }
