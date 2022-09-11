ec724bb03003a87aa301a312a0b8d3dd8e6c3c90
jennifer-ling <72683932+jennifer-ling@users.noreply.github.com>
2022-09-11 21:02:20 +0800

    add MST Prim ver.

---
+++ codebook/Graph/MST_Prim.cpp
@@ -0,0 +1,31 @@
+vector<pii> edges[MAXN];
+// edges[i]: edge(i, edges[i].second) with weight edges[i].first
+
+int Prim_MST(int N){
+  bool choose[N] = {0};
+  int count = 0, weight = 0;
+  priority_queue<pii, vector<pii>, greater<pii>> pq;
+  // choose one vertex
+  choose[0] = 1;
+  count ++;
+  // push its edges to pq
+  for(auto it : edges[0]){
+    pq.push(it);
+  }
+  // do it until pq empty
+  while(!pq.empty()){
+    auto add = pq.top();
+    pq.pop();
+    if(choose[add.second])
+      continue;
+    choose[add.second] = 1;
+    //printf("add: vertex %d, with %d\n", add.second, add.first);
+    count ++;
+    weight += add.first;
+    for(auto it : edges[add.second]){
+      pq.push(it);
+    }
+  }
+  if(count != N) return -1;
+  return weight;
+}
