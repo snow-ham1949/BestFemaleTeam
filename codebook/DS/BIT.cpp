88b943dcd1ea1c1424cd276162870d309e4319e7
jennifer-ling <72683932+jennifer-ling@users.noreply.github.com>
2022-09-15 15:40:00 +0800

    Put BIT into struct

--- codebook/DS/BIT.cpp
+++ codebook/DS/BIT.cpp
@@ -2,54 +2,53 @@
 // range update, point query: 差分
 // range update, range query: 要用兩個 BIT，一個維護差分D_i, 一個維護差分D_i * i
 
-typedef struct Tree{
+// new instace: 
+struct BIT{
 	ll *tree;
 	ll size;
-}BIT; 
 
-ll lowbit(ll x){
-	return x & (-x);
-}
+	ll lowbit(ll x){
+		return x & (-x);
+	}
 
-void init(BIT *ptr, ll size){
-	ptr->size = size;
-  ptr->tree = new ll[size+1]();
-}
+	void init(ll _size){
+		size = _size;
+		tree = new ll[size + 1]();		
+	}
 
-// add value on pos V
-void updateBIT(BIT *ptr, ll pos, ll value){
-	for(ll i = pos; i <= ptr->size; i += lowbit(i))
-		ptr->tree[i] += value;
-}
+	// add value on pos
+	void updateBIT(ll pos, ll value){
+		for(ll i = pos; i <= size; i += lowbit(i))
+			tree[i] += value;
+	}
 
-// build BIT tree V
-BIT *build(int size, int data[]){
-	BIT *newtree = new BIT;
-	init(newtree,size);
-	for(int i = 0; i < size; i++)
-		updateBIT(newtree,i+1,data[i]);
-	return newtree;
-}
+	void build_with_arr(ll _size, int data[]){
+		init(_size);
+		for(ll i = 0; i < size; i++){
+			updateBIT(i+1, data[i]);
+		}
+	}
 
-// get sum on [1 .. pos] V
-ll getsum(BIT *ptr, ll pos){
-	ll sum = 0;
-	for(ll i = pos; i > 0; i -= lowbit(i))
-		sum += ptr->tree[i];
-	return sum;
-}
+	// get sum on [1 .. pos]
+	ll getsum(ll pos){
+		ll sum = 0;
+		for(ll i = pos; i > 0; i -= lowbit(i))
+			sum += tree[i];
+		return sum;
+	}
 
-// get sum on [l..r] V
-ll rangesum(BIT *ptr, ll l, ll r){
-	return getsum(ptr,r)-getsum(ptr,l-1);
-}
+	// get sum on [l..r]
+	ll rangesum(ll l, ll r){
+		return getsum(r)-getsum(l-1);
+	}
 
-// for range update and point query
-// the array is preprocessed
-void updaterange(BIT *ptr, int l, int r, int k){
-    // Increase value at 'l' by 'k'
-    updateBIT(ptr, l, k);
- 
-    // Decrease value at 'r+1' by 'k'
-    updateBIT(ptr, r+1, -k);
-}
+	// for range update and point query
+	// the array is preprocessed
+	void updaterange(int l, int r, int k){
+			// Increase value at 'l' by 'k'
+			updateBIT(l, k);
+	
+			// Decrease value at 'r+1' by 'k'
+			updateBIT(r+1, -k);
+	}
+}; 
