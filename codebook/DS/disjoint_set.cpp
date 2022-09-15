51d9d4f887f064081c635d4abd33862ab9c38ee3
jennifer-ling <72683932+jennifer-ling@users.noreply.github.com>
2022-09-15 15:40:43 +0800

    Put DJS into struct

--- codebook/DS/disjoint_set.cpp
+++ codebook/DS/disjoint_set.cpp
@@ -2,24 +2,23 @@ struct disjoint_set{
 	struct disjoint_set * parent;
 	ll rank;
 	ll data;
+
+	void make_set(ll _data){
+		data = _data;
+		parent = this;
+		rank = 0;
+	}
+
+	disjoint_set *find_set(){
+		if(parent != this)
+			parent = parent->find_set();
+		return parent;
+	}
+ 
 };
 
-disjoint_set *make_set(ll data){
-	disjoint_set *newset  = new disjoint_set;
-	newset->data = data;
-	newset->parent = newset;
-	newset->rank = 0;
-  return newset;
-}
-
-disjoint_set *find_set(disjoint_set *x){
-	if(x->parent != x)
-		x->parent = find_set(x->parent);
-	return x->parent;
-}
-
 void Union(disjoint_set *a, disjoint_set *b){
-	disjoint_set *x = find_set(a), *y = find_set(b);
+	disjoint_set *x = a->find_set(), *y = b->find_set();
 	if(x == y)
 		return;
 	if(x->rank > y->rank)
@@ -32,4 +31,4 @@ void Union(disjoint_set *a, disjoint_set *b){
 	else
 		x->parent = y;
   return;
-} 
+}
