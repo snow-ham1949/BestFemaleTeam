ffacac6dab1eac8aff8d5b5804d4921c873fd7ac
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-09-17 20:56:59 +0800

    unified structure

--- codebook/DS/BIT.cpp
+++ codebook/DS/BIT.cpp
@@ -4,51 +4,51 @@
 
 // new instace: 
 struct BIT{
-	ll *tree;
-	ll size;
+  vector<ll> tree;
+ll size;
 
-	ll lowbit(ll x){
-		return x & (-x);
-	}
+  ll lowbit(ll x){
+    return x & (-x);
+  }
 
-	void init(ll _size){
-		size = _size;
-		tree = new ll[size + 1]();		
-	}
+  void init(ll _size){
+    size = _size;
+    tree.assign(n + 1, 0);
+  }
 
-	// add value on pos
-	void updateBIT(ll pos, ll value){
-		for(ll i = pos; i <= size; i += lowbit(i))
-			tree[i] += value;
-	}
+  // add value on pos
+  void update(ll pos, ll value){
+    for(ll i = pos; i <= size; i += lowbit(i))
+    tree[i] += value;
+  }
 
-	void build_with_arr(ll _size, int data[]){
-		init(_size);
-		for(ll i = 0; i < size; i++){
-			updateBIT(i+1, data[i]);
-		}
-	}
+  void build_with_arr(ll _size, int data[]){
+    init(_size);
+    for(ll i = 0; i < size; i++){
+      update(i+1, data[i]);
+    }
+  }
 
-	// get sum on [1 .. pos]
-	ll getsum(ll pos){
-		ll sum = 0;
-		for(ll i = pos; i > 0; i -= lowbit(i))
-			sum += tree[i];
-		return sum;
-	}
+  // get sum on [1 .. pos]
+  ll getsum(ll pos){
+    ll sum = 0;
+    for(ll i = pos; i > 0; i -= lowbit(i))
+      sum += tree[i];
+    return sum;
+  }
 
-	// get sum on [l..r]
-	ll rangesum(ll l, ll r){
-		return getsum(r)-getsum(l-1);
-	}
+  // get sum on [l..r]
+  ll rangesum(ll l, ll r){
+    return getsum(r)-getsum(l-1);
+  }
 
-	// for range update and point query
-	// the array is preprocessed
-	void updaterange(int l, int r, int k){
-			// Increase value at 'l' by 'k'
-			updateBIT(l, k);
-	
-			// Decrease value at 'r+1' by 'k'
-			updateBIT(r+1, -k);
-	}
+  // for range update and point query
+  // the array is preprocessed
+  void updaterange(int l, int r, int k){
+    // Increase value at 'l' by 'k'
+    update(l, k);
+
+    // Decrease value at 'r+1' by 'k'
+    update(r+1, -k);
+  }
 }; 
