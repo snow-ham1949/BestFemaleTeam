16727e72ee6ced3ee4a7c97bc00f9021b296569d
jennifer-ling <72683932+jennifer-ling@users.noreply.github.com>
2022-10-07 09:44:34 +0800

    Put into struct

--- codebook/DS/Trie_ptr.cpp
+++ codebook/DS/Trie_ptr.cpp
@@ -1,33 +1,34 @@
 struct Node{
   Node *child[CSIZE]={0};
   int EndOfWord = 0;
-};
+  string str;
 
-int getindex(char a){
-  return a-'a';
-}
+  int getindex(char a){
+    return a-'a';
+  }
 
-void insert(Node *T, string s){
-  Node *tmp = T;
-  int len = s.length();
-  for (int i = 0; i < len; i++)
-  {
+  void insert(string s){
+    Node *tmp = this;
+    int len = s.length();
+    for (int i = 0; i < len; i++)
+    {
+        int index = getindex(s[i]);
+        if (!tmp->child[index])
+            tmp->child[index] = new Node;
+        tmp = tmp->child[index];
+    }
+    tmp->EndOfWord ++;
+    tmp->str = s;
+  }
+
+  int find(string s){
+    Node *tmp = this;
+    int len = s.length();
+    for(int i = 0; i < len; i++){
       int index = getindex(s[i]);
-      if (!tmp->child[index])
-          tmp->child[index] = new Node;
+      if(!tmp->child[index]) return 0;
       tmp = tmp->child[index];
+    }
+    return tmp->EndOfWord;
   }
-  tmp->EndOfWord ++;
-}
-
-bool find(Node *T, string s){
-  Node *tmp = T;
-  int len = s.length();
-  for(int i = 0; i < len; i++){
-    int index = getindex(s[i]);
-    if(!tmp->child[index]) return false;
-    tmp = tmp->child[index];
-  }
-  if(!tmp->EndOfWord) return false;
-  return true;
-}
+};
