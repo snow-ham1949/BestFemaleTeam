0305fa33f1e9decfebc69a6af008889bad45d7de
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-10-15 19:12:07 +0800

    add debug template

---
+++ codebook/Basic/debug.cpp
@@ -0,0 +1,11 @@
+#ifdef debug
+void trace_() { cerr << "\n"; }
+template <typename T1, typename... T2>
+void trace_(T1 t1, T2... t2) {
+  cerr << ' ' << t1;
+  trace_(t2...);
+}
+#define trace(...) cerr << "[" << #__VA_ARGS__ << "] :", trace_(__VA_ARGS__);
+#else
+#define trace(...) 49
+#endif
