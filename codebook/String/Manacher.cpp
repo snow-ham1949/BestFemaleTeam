f4382867947130e25632c5555a7ea2ffc793b72a
Yun-Fang Li <59408960+snow-ham1949@users.noreply.github.com>
2023-05-20 21:08:01 +0800

    Update Manacher.cpp

--- codebook/String/Manacher.cpp
+++ codebook/String/Manacher.cpp
@@ -25,3 +25,34 @@ int Manacher(string tmp)
   }
   return max(ans, (x - 1) / 2 * 2 + 1);
 }
+
+// return longest palindrome in string tmp
+vector<int> manacherOdd(string s) {
+  int n = SZ(s);
+  s = "$" + s + "^";
+  vector<int> p(n+2);
+  int l = 1, r = 1;
+  for (int i = 1; i <= n; i++) {
+    p[i] = max(0, min(r - i, p[l + r - i]));
+    while (s[i - p[i]] == s[i + p[i]]) p[i]++;
+    if (i + p[i] > r) {
+      l = i - p[i]; 
+      r = i + p[i];
+    }
+  }
+  return vector<int>(begin(p)+1,end(p)-1);
+}
+ 
+string manacher(string s) {
+  string t;
+  for(auto c : s) t += string("#") + c;
+  vector<int> ans = manacherOdd(t + "#");
+  int maxid = 1, maxlen = ans[1];
+  for (int i = 1; i < SZ(ans) - 1; i++) {
+    if (ans[i] > maxlen) { 
+      maxlen = ans[i]; 
+      maxid = i;
+    }
+  }
+  return s.substr(maxid / 2 - (maxlen - 1) / 2, maxlen - 1);
+}
