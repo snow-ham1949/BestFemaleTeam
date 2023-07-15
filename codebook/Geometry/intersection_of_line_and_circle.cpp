23af0e463c9d933be557635c48ca896943ee0f8e
snow-ham1949 <b08502041@ntu.edu.tw>
2023-07-15 20:44:22 +0800

    add intersection of line and circle

---
+++ codebook/Geometry/intersection_of_line_and_circle.cpp
@@ -0,0 +1,9 @@
+// return intersection coordinate
+vector<pdd> circleLine(pdd c, double r, pdd a, pdd b) {
+  pdd p = a + (b - a) * dot(c - a, b - a) / abs2(b - a);
+  double s = cross(b - a, c - a), h2 = r * r - s * s / abs2(b - a);
+  if (h2 < 0) return {};
+  if (h2 == 0) return {p};
+  pdd h = (b - a) / abs(b - a) * sqrt(h2);
+  return {p - h, p + h};
+}
