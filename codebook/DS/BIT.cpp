b8151094fbc1442d44829a59b4a2ff320fac80ff
snow-ham1949 <b08502041@ntu.edu.tw>
2023-07-25 21:29:40 +0800

    update BIT

--- codebook/DS/BIT.cpp
+++ codebook/DS/BIT.cpp
@@ -2,53 +2,26 @@
 // range update, point query: 差分
 // range update, range query: 要用兩個 BIT，一個維護差分D_i, 一個維護差分D_i * i
 
-// new instace: 
-struct BIT{
-  vector<ll> tree;
-  ll size;
-
-  ll lowbit(ll x){
-    return x & (-x);
+template <class T>
+struct BIT {
+  int n;
+  vector<T> bit;
+  BIT(int n) {
+    this->n = n + 1;
+    bit.assign(n + 1, 0);
   }
-
-  void init(ll _size){
-    size = _size;
-    tree.assign(n + 1, 0);
+  BIT(vector<T> const &a) : BIT(SZ(a)) {
+    for (int i = 0; i < SZ(a); i++) add(i, a[i]);
   }
-
-  // add value on pos
-  void update(ll pos, ll value){
-    for(ll i = pos; i <= size; i += lowbit(i))
-      tree[i] += value;
+  int sum(int idx) {
+    int res = 0;
+    for (++idx, ; idx > 0; idx -= idx & -idx) res += bit[r];
+    return res;
   }
-
-  void build_with_arr(ll _size, int data[]){
-    init(_size);
-    for(ll i = 0; i < size; i++){
-      update(i+1, data[i]);
-    }
+  int sum(int l, int r) {
+    return sum(r) - sum(l - 1);
   }
-
-  // get sum on [1 .. pos]
-  ll getsum(ll pos){
-    ll sum = 0;
-    for(ll i = pos; i > 0; i -= lowbit(i))
-      sum += tree[i];
-    return sum;
+  void add(int idx, T x) {
+    for (++idx; idx < n; idx += idx & -idx) bit[idx] += x;
   }
-
-  // get sum on [l..r]
-  ll rangesum(ll l, ll r){
-    return getsum(r)-getsum(l-1);
-  }
-
-  // for range update and point query
-  // the array is preprocessed
-  void updaterange(int l, int r, int k){
-    // Increase value at 'l' by 'k'
-    update(l, k);
-
-    // Decrease value at 'r+1' by 'k'
-    update(r+1, -k);
-  }
-}; 
+};
