8ab59c8e57317671ae708325ad2eb8ce9ee46503
snow-ham1949 <b08502041@ntu.edu.tw>
2023-07-29 23:44:39 +0800

    update DJS

--- codebook/DS/disjoint_set.cpp
+++ codebook/DS/disjoint_set.cpp
@@ -1,34 +1,19 @@
-struct disjoint_set{
-	struct disjoint_set * parent;
-	ll rank;
-	ll data;
-
-	void make_set(ll _data){
-		data = _data;
-		parent = this;
-		rank = 0;
-	}
-
-	disjoint_set *find_set(){
-		if(parent != this)
-			parent = parent->find_set();
-		return parent;
-	}
- 
-};
-
-void Union(disjoint_set *a, disjoint_set *b){
-	disjoint_set *x = a->find_set(), *y = b->find_set();
-	if(x == y)
-		return;
-	if(x->rank > y->rank)
-		y->parent = x;
-	else if (x->rank == y->rank)
-	{
-		y->parent = x;
-		x->rank ++;
-	}
-	else
-		x->parent = y;
-  return;
-}
+struct DJS {
+  int n;
+  vector<int> p, sz;
+  void init(_n) {
+    n = _n;
+    p.resize(n);
+    sz.assign(n, 1);
+    iota(ALL(p), 0);
+  }
+  int find(int x) {
+    return p[x] == x ? x : p[x] = find(p[x]);
+  }
+  void _union(int x, int y) {
+    int px = find(x), py = find(y);
+    if (sz[px] > sz[py]) swap(px, py);
+    p[x] = y;
+    sz[py] += sz[px];
+  }
+} djs;
