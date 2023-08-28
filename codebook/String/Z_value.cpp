30cb34f17c732d4c2b69038655b521e9e48e982a
Yun-Fang Li <59408960+snow-ham1949@users.noreply.github.com>
2023-08-28 22:35:34 +0800

    Update Z_value.cpp

--- codebook/String/Z_value.cpp
+++ codebook/String/Z_value.cpp
@@ -1,9 +1,12 @@
 int z[MAXN];
-void getz(string s){
-    int l=0;
-    for(int i = 0; i < SZ(s); i++){
-        if(l+z[l]>i) z[i]=min(z[i-l],l+z[l]-i);
-        while(i+z[i]<SZ(s)&&s[z[i]]==s[i+z[i]]) z[i]++;
-        if(i+z[i]>l+z[l]) l=i;
-    }
+void make_z(const string &s) {
+  int l = 0, r = 0;
+  z[0] = SZ(s);
+  for (int i = 1; i < SZ(s); ++i) {
+    for (z[i] = max(0, min(r - i + 1, z[i - l]));
+         i + z[i] < SZ(s) && s[i + z[i]] == s[z[i]];
+         ++z[i])
+      ;
+    if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+  }
 }
