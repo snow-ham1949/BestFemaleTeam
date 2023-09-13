64cc21135d47d58937e24587bfa1ddcce7be2be3
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-09-13 21:46:47 +0800

    update Pollard Rho

--- codebook/Math/Pollard_Rho.cpp
+++ codebook/Math/Pollard_Rho.cpp
@@ -1,25 +1,48 @@
-long long int mult(long long int a, long long int b, long long int m){
-	return (a*b)%m;
+ll mul(ll a, ll b, ll mod) { return (__int128)a * b % mod; }
+bool Miller_Rabin(ll a, ll n) {
+  if((a = a % n) == 0) return 1;
+  if((n & 1) ^ 1) return n == 2;
+  ll tmp = (n - 1) / ((n - 1) & (1 - n));
+  ll t = __lg(((n - 1) & (1 - n))), x = 1;
+  for(; tmp; tmp >>= 1, a = mul(a, a, n))
+    if(tmp & 1) x = mul(x, a, n);
+  if(x == 1 || x == n - 1) return 1;
+  while(--t)
+    if((x = mul(x, x, n)) == n - 1) return 1;
+  return 0;
 }
-long long int f(long long int x, long long int m){//fake random function
-	return (mult(x, x, m)+1)%m;
+// n < 4,759,123,141      3 : 2, 7, 61
+// n < 1,122,004,669,633  4 : 2, 13, 23, 1662803
+// n < 3,474,749,660,383  6 : pirmes <= 13
+// n < 2^64             7 :
+// 2, 325, 9375, 28178, 450775, 9780504, 1795265022
+int prime(ll p) {
+    const static int base[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  for (int i = 0; i < 7; ++i)
+    if (!Miller_Rabin(base[i], p))
+      return 0;
+  return 1;
 }
-long long int pollard_rho(long long int n){
-	if(n%2==0){
-		return 2;
-	}
-	long long int xi = 2, xj = 2;
-	int i = 1, j = 1;
-	while(true){
-		j++;
-		xj = f(xj, n);
-		int d = __gcd(abs(xi-xj), n);
-		if(d!=1){
-			return d;
-		}
-		if(j==i*2){
-			i*=2;
-			xi = xj;
-		}
-	}
+
+map<ll, int> cnt;
+void PollardRho(ll n) {
+  if (n == 1) return;
+  if (prime(n)) return ++cnt[n], void();
+  if (n % 2 == 0) return PollardRho(n / 2), ++cnt[2], void();
+  ll x = 2, y = 2, d = 1, p = 1;
+  #define f(x, n, p) ((mul(x, x, n) + p) % n)
+  while (true) {
+    if (d != n && d != 1) {
+      PollardRho(n / d);
+      PollardRho(d);
+      return;
+    }
+    if (d == n) ++p;
+    x = f(x, n, p); y = f(f(y, n, p), n, p);
+    d = gcd(abs(x - y), n);
+  }
 }
+
+// usage:
+// PollardRho(x)
+// for (auto [num, c] : cnt) x has num^c
