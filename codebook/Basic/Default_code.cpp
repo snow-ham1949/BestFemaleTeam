f5c0784f8a6343443fbe493f3339c079e78a2eca
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-09-13 19:59:39 +0800

    update default code

--- codebook/Basic/Default_code.cpp
+++ codebook/Basic/Default_code.cpp
@@ -4,14 +4,6 @@ using namespace std;
 
 #define int long long
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-#define SZ(a) (int)(a).size()
-#define ALL(v) (v).begin(), (v).end()
-#define X first
-#define Y second
-
 void solve() {
   
 }
