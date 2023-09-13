9214ff3a7560004c5b09cbafebff43bdfb40745a
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-09-13 20:00:54 +0800

    update testing py

--- codebook/Basic/test.py
+++ codebook/Basic/test.py
@@ -1,23 +1,12 @@
 n = 1000000
 
 def updatebit(BITTree , n , i ,v):
-
-	# index in BITree[] is 1 more than the index in arr[]
 	i += 1
-
-	# Traverse all ancestors and add 'val'
 	while i <= n:
-
-		# Add 'val' to current node of BI Tree
 		BITTree[i] += v
-
-		# Update index to that of parent in update View
 		i += i & (-i)
 
-
-# Create and initialize BITree[] as 0
 BITTree = [0]*(n+1)
 
-# Store the actual values in BITree[] using update()
 for i in range(n):
   updatebit(BITTree, n, i, i)
