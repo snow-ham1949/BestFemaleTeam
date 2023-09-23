92a74e527c22928d237406ea79732321ac607d9a
Yun-Fang Li <59408960+snow-ham1949@users.noreply.github.com>
2023-09-23 15:28:48 +0800

    Update disjoint_set_undo.cpp

--- codebook/DS/disjoint_set_undo.cpp
+++ codebook/DS/disjoint_set_undo.cpp
@@ -10,46 +10,46 @@ int compo;
 int pts[MX*2],in=0;
 
 int parent(int n){
-    if(rp[n]==n)return n;
-    return rp[n]=parent(rp[n]);
+	if(rp[n]==n)return n;
+	return rp[n]=parent(rp[n]);
 }
 
 // additionally storing parent which is connected to another parents
 void setUp(int a,int b){
-    a = parent(a);
-    b = parent(b);
-    if(a==b){
-        pts[++in]=-1;
-        return;
-    }
-    if(sz[a]<sz[b]){
-        rp[a] = rp[b];
-        sz[b] += sz[a];
-        pts[++in]=a;
-    }
-    else{
-        rp[b] = rp[a];
-        sz[a] += sz[b];
-        pts[++in] = b;;
-    }
-    compo--;
+	a = parent(a);
+	b = parent(b);
+	if(a==b){
+		pts[++in]=-1;
+		return;
+	}
+	if(sz[a]<sz[b]){
+		rp[a] = rp[b];
+		sz[b] += sz[a];
+		pts[++in]=a;
+	}
+	else{
+		rp[b] = rp[a];
+		sz[a] += sz[b];
+		pts[++in] = b;
+	}
+	compo--;
 }
 
 void undo(){
-    if(!in) return;
-    int n = pts[in--];
-    if(n!=-1) {
-        sz[parent(rp[n])] -= sz[n];
-        rp[n]=n;
-        compo++;
-    }
+	if(!in) return;
+	int n = pts[in--];
+	if(n!=-1) {
+		sz[parent(rp[n])] -= sz[n];
+		rp[n]=n;
+		compo++;
+	}
 }
 
 void init(int n){
-    in=0;
-    for(int i=0;i<=MX;i++){
-        rp[i]=i;
-        sz[i]=1;
-    }
-    compo=n;
+	in=0;
+	for(int i=0;i<=MX;i++){
+		rp[i]=i;
+		sz[i]=1;
+	}
+	compo=n;
 }
