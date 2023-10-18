02a8f79824fcfb81139407c4b96b5f640505dfdf
ntsamaaa <59410559+ntsamaaa@users.noreply.github.com>
2023-10-18 21:58:03 +0800

    Update li_chao.cpp

--- codebook/DS/li_chao.cpp
+++ codebook/DS/li_chao.cpp
@@ -1,24 +1,20 @@
-using uint = unsigned int;
 using ll = long long;
-using ull = unsigned long long;
 constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
-template <class T> using V = vector<T>;
-template <class T> using VV = V<V<T>>;
 
 constexpr ll INF = 3 * TEN(18);
 struct LiChaoTree {
     using L = pair<ll, ll>;  // l.first * x + l.second
     int sz;
-    V<L> data;
-    V<ll> xs;
+    vector<L> data;
+    vector<ll> xs;
     static ll eval(L l, ll x) { return l.first * x + l.second; }
-    LiChaoTree(V<ll> _xs) : xs(_xs) { // xs stores all x appears in this problem
+    LiChaoTree(vector<ll> _xs) : xs(_xs) { // xs stores all x appears in this problem
         int n = int(xs.size());
         int lg = 1;
         while ((1 << lg) < n) lg++;
         sz = 1 << lg;
         while (int(xs.size()) < sz) xs.push_back(TEN(9));
-        data = V<L>(2 * sz, L(0, 3 * TEN(18)));
+        data = vector<L>(2 * sz, L(0, 3 * TEN(18)));
     }
     void add(L line, int l, int r) { // add({a, b}, l, r): add a segment f(x) = a*x+b, x in [l, r]
         l = lower_bound(xs.begin(), xs.end(), l) - xs.begin();
@@ -41,7 +37,7 @@ struct LiChaoTree {
         if (qr <= l || r <= ql) {
             return;
         } else if (ql <= l && r <= qr) {
-            int mid = (l + r) / 2;
+            int mid = (l + r) >> 1;
             ll mx = xs[mid];
             if (eval(line, mx) < eval(data[k], mx)) {
                 swap(line, data[k]);
@@ -53,7 +49,7 @@ struct LiChaoTree {
                 add(line, ql, qr, mid, r, 2 * k + 1);
             }
         } else {
-            int mid = (l + r) / 2;
+            int mid = (l + r) >> 1;
             add(line, ql, qr, l, mid, 2 * k);
             add(line, ql, qr, mid, r, 2 * k + 1);
         }
