29f17a18238401ecd7ab299d2febab20f1aa7de4
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-10-20 00:08:06 +0800

    add bipartite matching using Dinic note

--- codebook/Else/note.cpp
+++ codebook/Else/note.cpp
@@ -1,9 +1,17 @@
-articulation point & bridge: if deleted, then connected component ++
-tree centroid: if deleted, then T will be separated into multiple trees, where nodes of each <= n / 2
-min cut: 
-  1. a cut such that all nodes can be separated into two sets S, T = V - S, and the capacity is minimum
-  2. how to find
+1. articulation point & bridge
+  if deleted, then connected component ++
+2. tree centroid
+  if deleted, then T will be separated into multiple trees, where nodes of each <= n / 2
+3. min cut: 
+   - a cut such that all nodes can be separated into two sets S, T = V - S, and the capacity is minimum
+   - how to find
      do flow
      block cap = 0 edge
      bfs from s, can reach -> S, can't reach -> T
-
+4. bipartite matching using dinic
+  - after doing dinic.maxflow
+  - for (int i = 1; i <= m; i++) {
+      for (auto [to, rev, flow, cap] : dinic.G[i]) {
+        if (flow == 1) cout << i << " " << to << "\n";
+      }
+    }
