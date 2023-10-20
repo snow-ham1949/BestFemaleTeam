6e2403d48f2ceeb6847c59a2feb563019d6031ff
jennifer-ling <72683932+jennifer-ling@users.noreply.github.com>
2023-10-20 10:55:49 +0800

    Update Dinic.cpp

--- codebook/Flow/Dinic.cpp
+++ codebook/Flow/Dinic.cpp
@@ -1,7 +1,7 @@
 const int N = 200 + 5; // number of vertices
 const ll INF = (1ll << 60) - 1;
 #define pb push_back
-
+// O(sqrt(V)E) for bipartite
 struct Dinic { // 0-base
   struct edge {
     int to, rev;
